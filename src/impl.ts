313ab31fe4c04eef71ba149c45489bd3110a77be
🌌 <Time.Rain.Star.Sky@Gmail.com>
2025-08-12 21:18:38 +0800

    细节优化

--- src/impl.ts
+++ src/impl.ts
@@ -1,10 +1,10 @@
-import { setTimeout } from "node:timers/promises"
 import * as inquirer from "@inquirer/prompts"
 import icqq from "icqq"
 import { makeLogger } from "philia/logger"
 import * as Common from "philia/project/project/common.js"
 import * as Philia from "philia/project/project/philia.js"
 import { selectArray } from "philia/util/tui.js"
+import Event from "./event/index.js"
 
 export interface IConfig extends Common.IConfig {
   name: "ICQQ"
@@ -24,8 +24,7 @@ export class Project extends Common.Project {
     this.philia = new Philia.Project(config.philia)
     this.client = new icqq.Client({ data_dir: `data/${config.uin}`, ...config.config })
     this.client.logger = makeLogger("ICQQ")
-    for (const i in Project.listener)
-      this.client.on(i, Project.listener[i as keyof typeof Project.listener]!.bind(this))
+    for (const i in Event) this.client.on(i, Event[i as keyof typeof Event].bind(this))
   }
 
   static async createConfig(name: IConfig["name"]) {
@@ -154,166 +153,6 @@ export class Project extends Common.Project {
     } catch (err) {
       throw TypeError("滑动验证代理地址格式错误", { cause: err })
     }
-  }
-
-  static listener: Partial<icqq.EventMap> = {
-    async "system.login.qrcode"(this: Project, event) {
-      this.manager.notice.set("扫码登录", `扫码登录：${event.image}`)
-      for (;;) {
-        await setTimeout(3000)
-        const { retcode } = await this.client.queryQrcodeResult()
-        switch (retcode) {
-          case 0:
-            return this.client.qrcodeLogin()
-          case 17:
-            return this.manager.notice.set("登录错误", "二维码已过期")
-          case 54:
-            return this.manager.notice.set("登录错误", "扫码登录取消")
-        }
-      }
-    },
-    "system.login.slider"(this: Project, event) {
-      this.manager.notice.set(
-        "滑动验证",
-        `Bot 与浏览器处于同一网络下推荐网页，否则网页反代\n\n` +
-          `网页反代验证：网页反代\n` +
-          `网页验证：网页\n` +
-          `手动验证：ticket\n` +
-          event.url,
-        msg => {
-          this.handleLoginSlider(event, msg)
-        },
-      )
-    },
-    "system.login.device"(this: Project, event) {
-      this.manager.notice.set(
-        "设备锁验证",
-        `请选择设备锁验证方式\n` +
-          `短信验证：输入短信\n` +
-          `扫码验证：扫码完成后，继续登录\n` +
-          event.url,
-        msg => {
-          this.handleLoginDevice(msg)
-        },
-      )
-    },
-    "system.login.auth"(this: Project, event) {
-      this.manager.notice.set("身份验证", `请完成身份验证后，继续登录\n${event.url}`, () => {
-        this.client.login()
-      })
-    },
-    "system.login.error"(this: Project, event) {
-      this.manager.notice.set("登录错误", `${event.message}(${event.code})\n`)
-    },
-    "system.offline"(this: Project, event) {
-      this.manager.notice.set("账号下线", event.message)
-      return this.philia.stop()
-    },
-    "system.online"(this: Project) {
-      return this.philia.start()
-    },
-  }
-
-  async handleLoginDevice(msg: string) {
-    if (msg === "短信") {
-      await this.client.sendSmsCode()
-      this.manager.notice.set("短信验证", "短信已发送，请输入验证码", msg => {
-        this.client.submitSmsCode(msg)
-      })
-    } else this.client.login()
-  }
-
-  async handleLoginSlider(event: Parameters<icqq.EventMap["system.login.slider"]>[0], msg: string) {
-    const fnc = {} as { ticket: () => string | Promise<string>; close: () => void }
-    switch (msg) {
-      case "网页反代": {
-        const ws = new WebSocket(this.config.slider)
-        const wsSend = (data: object) => {
-          this.logger.debug(`发送 ${this.config.slider}`, data)
-          ws.send(JSON.stringify(data))
-        }
-        let ticket: string | Error
-        fnc.ticket = () => {
-          if (ticket instanceof Error) throw ticket
-          return ticket
-        }
-        fnc.close = ws.close.bind(ws)
-        ws.onclose = () => {
-          this.logger.debug(`连接关闭 ${this.config.slider}`)
-          ticket ??= Error(`连接关闭 ${this.config.slider}`)
-        }
-        ws.onerror = error => {
-          this.logger.debug(`连接错误 ${this.config.slider}`, error)
-          ticket ??= Error(`连接错误 ${this.config.slider}`, { cause: error })
-          fnc.close()
-        }
-        ws.onopen = () => {
-          wsSend({ type: "register", payload: { url: event.url } })
-          this.manager.notice.set("滑动验证地址", String(this.config.slider))
-        }
-        ws.onmessage = async msg => {
-          try {
-            const data = JSON.parse(msg.data)
-            this.logger.debug([`收到 ${data.payload.ticket}`, data])
-            switch (data.type) {
-              case "ticket":
-                ticket = data.payload.ticket
-                fnc.close()
-                break
-              case "handle": {
-                const { url, ...opts } = data.payload
-                const req = await fetch(url, opts)
-                data.payload = {
-                  result: Buffer.from(await req.arrayBuffer()).toString("base64"),
-                  headers: Object.fromEntries(req.headers.entries()),
-                }
-                wsSend(data)
-                break
-              }
-              default:
-                this.logger.info(data)
-            }
-          } catch (err) {
-            this.logger.error(err)
-          }
-        }
-        break
-      }
-      case "网页": {
-        await fetch(this.config.slider, {
-          method: "POST",
-          headers: { "Content-Type": "application/json" },
-          body: JSON.stringify({ url: event.url }),
-        })
-        this.manager.notice.set("滑动验证地址", String(this.config.slider))
-
-        fnc.ticket = async () => {
-          const res = await (
-            await fetch(this.config.slider, {
-              method: "POST",
-              headers: { "Content-Type": "application/json" },
-              body: JSON.stringify({ submit: this.config.uin }),
-            })
-          ).json()
-          return res.data?.ticket
-        }
-        break
-      }
-      default:
-        return this.client.submitSlider(msg)
-    }
-    try {
-      for (let i = 0; i < 60; i++) {
-        await setTimeout(3000)
-        const ticket = await fnc.ticket()
-        if (ticket) return this.client.submitSlider(ticket)
-      }
-    } catch (err) {
-      this.logger.error(err)
-      return this.manager.notice.set("登录错误", "滑动验证错误")
-    }
-    if (fnc.close) fnc.close()
-    return this.manager.notice.set("登录错误", "滑动验证超时")
   }
 
   start() {
